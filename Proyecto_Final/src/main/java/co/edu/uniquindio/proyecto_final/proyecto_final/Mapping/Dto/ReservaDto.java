23e0b74ef42a8c1346899864aa2695b362ecfdc2
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-03-26 16:29:21 -0500

    dto

--- Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Mapping/Dto/ReservaDto.java
+++ Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Mapping/Dto/ReservaDto.java
@@ -1,5 +1,14 @@
 package co.edu.uniquindio.proyecto_final.proyecto_final.Mapping.Dto;
 
-public record ReservaDto() {
+import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Evento;
+import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Usuario;
 
+import java.util.Date;
+
+public record ReservaDto() {
+    static int id;
+    static Usuario usuario;
+    static Evento evento;
+    static Date fechaSolicitud;
+    static String estado;
 }
