b3a994f6e922cca9f8d95794dd98e3028fe2c5fb
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-03-27 17:55:19 -0500

    mapper

--- Proyecto_Final/src/main/java/module-info.java
+++ Proyecto_Final/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.uniquindio.proyecto_final.proyecto_final {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mapstruct;
 
 
     opens co.edu.uniquindio.proyecto_final.proyecto_final to javafx.fxml;
