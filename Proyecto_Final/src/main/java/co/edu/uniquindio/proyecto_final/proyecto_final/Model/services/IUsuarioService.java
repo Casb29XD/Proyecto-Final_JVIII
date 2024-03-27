231ae1969f2202188c63d705a898b52b714b7bcd
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-03-27 15:26:49 -0500

    model

--- Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Model/services/IUsuarioService.java
+++ Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Model/services/IUsuarioService.java
@@ -1,7 +1,6 @@
 package co.edu.uniquindio.proyecto_final.proyecto_final.Model.services;
 
 import co.edu.uniquindio.proyecto_final.proyecto_final.Exceptions.UsuarioException;
-import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Empleado;
 import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Reserva;
 import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Usuario;
 
@@ -10,7 +9,7 @@ import java.util.ArrayList;
 public interface IUsuarioService {
     public Usuario crearUsuario(int id, String nombre, String correo, ArrayList<Reserva> reserva)throws UsuarioException;
     public boolean eliminarUsuario(int id)throws UsuarioException;
-    public boolean actualizarUsuario(int id, Empleado empleado)throws UsuarioException;
+    public boolean actualizarUsuario(int id, Usuario usuario)throws UsuarioException;
     public boolean verificarUsuario(int id)throws UsuarioException;
     public Usuario obtenerUsuario(int id)throws UsuarioException;
     public ArrayList<Usuario> obtenerUsuarios();
