adbf5e9ec35fe56e1621e8ef52ec67adb0c42a8e
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-03-27 18:39:41 -0500

    mapper

--- Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/ModelFactoryController.java
+++ Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/ModelFactoryController.java
@@ -2,10 +2,40 @@ package co.edu.uniquindio.proyecto_final.proyecto_final.Controller;
 
 import co.edu.uniquindio.proyecto_final.proyecto_final.Controller.Service.IModelFactoryService;
 import co.edu.uniquindio.proyecto_final.proyecto_final.Mapping.Dto.UsuarioDto;
+import co.edu.uniquindio.proyecto_final.proyecto_final.Mapping.Mappers.SgreMapper;
+import co.edu.uniquindio.proyecto_final.proyecto_final.Model.Sgre;
+import co.edu.uniquindio.proyecto_final.proyecto_final.Utils.SgreUtils;
 
 import java.util.List;
 
 public class ModelFactoryController implements IModelFactoryService {
+    Sgre sgre;
+    SgreMapper mapper = SgreMapper.INSTANCE;
+
+    public static class SingletonHolder{
+        private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
+    }
+    public static ModelFactoryController getInstance() {
+        return SingletonHolder.eINSTANCE;
+    }
+
+    public ModelFactoryController() {
+        System.out.println("invocación clase singleton");
+        cargarDatosBase();
+    }
+
+    private void cargarDatosBase() {
+        sgre = SgreUtils.InicializarDatos();
+    }
+
+    public Sgre getSgre() {
+        return sgre;
+    }
+
+    public void setSgre(Sgre sgre) {
+        this.sgre = sgre;
+    }
+
     @Override
     public List<UsuarioDto> obtenerUsuarios() {
         return null;
