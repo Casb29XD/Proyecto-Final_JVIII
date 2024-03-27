840171c23e42371dbafabc99c44c1a4aaa22517d
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-03-27 17:26:58 -0500

    lol

--- Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/ModelFactoryController.java
+++ Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/ModelFactoryController.java
@@ -1,4 +1,28 @@
 package co.edu.uniquindio.proyecto_final.proyecto_final.Controller;
 
-public class ModelFactoryController {
+import co.edu.uniquindio.proyecto_final.proyecto_final.Controller.Service.IModelFactoryService;
+import co.edu.uniquindio.proyecto_final.proyecto_final.Mapping.Dto.UsuarioDto;
+
+import java.util.List;
+
+public class ModelFactoryController implements IModelFactoryService {
+    @Override
+    public List<UsuarioDto> obtenerUsuarios() {
+        return null;
+    }
+
+    @Override
+    public boolean agregarUsuario(UsuarioDto usuario) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminarUsuario(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean actualizarUsuario(int id, UsuarioDto usuario) {
+        return false;
+    }
 }
--- Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/Service/IModelFactoryService.java
+++ Proyecto_Final/src/main/java/co/edu/uniquindio/proyecto_final/proyecto_final/Controller/Service/IModelFactoryService.java
@@ -1,4 +1,12 @@
 package co.edu.uniquindio.proyecto_final.proyecto_final.Controller.Service;
 
+import co.edu.uniquindio.proyecto_final.proyecto_final.Mapping.Dto.UsuarioDto;
+
+import java.util.List;
+
 public interface IModelFactoryService {
+    List<UsuarioDto> obtenerUsuarios();
+    boolean agregarUsuario(UsuarioDto usuario);
+    boolean eliminarUsuario(int id);
+    boolean actualizarUsuario(int id, UsuarioDto usuario);
 }
